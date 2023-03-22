1ba9ca6e3bcd9f950e9382dcc450acb2a7d3e120
SarahMA19 <sarahgreisen@gmail.com>
2023-03-22 15:28:54 -0700

    second

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,12 @@
 from app import app 
 
 
+from flask import render_template
+
 @app.route('/')
 def homePage():
-    return 
+    return render_template('index.html')
 
 @app.route('/login')
 def loginPage():
-    return 
+    return render_template('login.html')
